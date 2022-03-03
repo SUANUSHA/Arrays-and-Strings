79c07e9108ba528112e77a0894eff9ca21131751
SUANUSHA <98738330+SUANUSHA@users.noreply.github.com>
2022-03-03 16:22:59 +0530

    anu

---
+++ second largest element.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+void main()
+{
+    int a[50];
+    int n,i,large,s_large;
+    printf("\n Enter number of elements: ");
+    scanf("%d",&n);
+     
+    printf("\n Enter the elements: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+     
+    large=s_large=a[0];
+     
+    for(i=1;i<n;i++)
+    {
+        if(large<a[i])
+        {
+            s_large=large;
+            large=a[i];
+        }
+        else if(s_large<a[i] && a[i]!=large)
+        {
+            s_large=a[i];
+        }
+    }
+     
+    printf("\n The Second Largest Element in the given Array: %d", s_large);
+    getch();
+}
